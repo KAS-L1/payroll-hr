618724320217bbd015e21e9bbb0930a1c5ec60f0
Jay Melendrez <joerge30@gmail.com>
2025-02-23 11:23:51 +0800

    Add error pages

---
+++ page/404.php
@@ -0,0 +1,6 @@
+<div class="page-content min-h-screen flex items-center justify-center bg-gray-100">
+    <div class="text-center">
+        <h1 class="text-3xl font-extrabold uppercase !leading-snug text-primary md:text-4xl">404</h1>
+        <p class="text-base font-bold leading-normal text-white-dark">Page Not Found</p>
+    </div>
+</div>
---
+++ public/403.php
@@ -0,0 +1,11 @@
+<?php include_once("public/_template/Header.php") ?>
+
+
+<div class="page-content min-h-screen flex items-center justify-center bg-gray-100">
+    <div class="text-center">
+        <h1 class="text-3xl font-extrabold uppercase !leading-snug text-primary md:text-4xl">403</h1>
+        <p class="text-base font-bold leading-normal text-white-dark">Unauthorized Access token is expired</p>
+    </div>
+</div>
+
+<?php include_once("public/_template/Footer.php") ?>
